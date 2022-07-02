0ad2cab5ae7fc85b3b75e7f3b7a04ccd0b1afc34
Elan Nurjailani <elan.nrj@gmail.com>
2022-07-02 07:01:18 +0700

    feat(user): implement user's route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var user = require('../controller/users')
+const { validate } = require('../middlewares')
+const validation = require('../validations')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', user.getUsers)
+
+/* CREATE a user. */
+router.post('/', validate(validation.userValidate), user.createUser)
+
+/* GET a user. */
+router.get('/:id', user.getUser)
 
 module.exports = router;
